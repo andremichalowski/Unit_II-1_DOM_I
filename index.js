6be3758e3ea6dfba01d69fc8fcebcd08fde44c01
amichalo421 <andre.michalowski@gmail.com>
2020-08-03 12:36:42 -0400

    Project_completed

--- index.js
+++ index.js
@@ -10,36 +10,41 @@
   Request 1: Log the result of every `a` element on the page. The result should be an `HTMLCollection`
 */
 
-//const anchorTags;
+const anchorTags = document.getElementsByTagName('a');
 //console.log(anchorTags);
 
 /* 
   Request 2: Log the result of every `a` element on the page. The result should be a `NodeList`
 */
 
-// const moreAnchorTags;
+const moreAnchorTags = document.quertySelectorAll('a');
 // console.log(moreAnchorTags);
 
 /* 
   Request 3: Locate the ID in the HTML. Log the element that the ID is on.
 */
 
-// const customID;
+const customID = document.getElementById('first-heading');
+// console.log(customID);
+
+// querySelector()
+
+// const customID = document.querySelector('#first-heading');
 // console.log(customID);
 
 /* 
   Request 4: Log the first class that matches ".main-content"
 */
 
-// const mainContent;
+const mainContent = document.querySelector('.main-content');
 // console.log(mainContent);
 
 /* 
   Request 5: Log the third element with a class of ".nav-item"
 */
 
-// const thirdNavItem;
-// console.log(thirdNavItem);
+const thirdNavItem = document.querySelectorAll('.nav-item');
+// console.log(thirdNavItem[2]);
 
 
 
